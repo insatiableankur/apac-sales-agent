c1bfcb8cc34aa1966081ca8a90248ae435982f03
Ankur Sehgal <ankursehgal@192.168.1.5>
2026-03-05 22:39:34 +0800

    Add delay between API calls

--- src/App.jsx
+++ src/App.jsx
@@ -975,7 +975,7 @@ export default function APACSalesAgent() {
       let liveIntel = "";
       try {
         setAnalyzeStep(0);
-        liveIntel = await searchCompanyIntel(form.company, form.market, form.industry);
+        liveIntel = await searchCompanyIntel(form.company, form.market, form.industry); await new Promise(r => setTimeout(r, 2000));
       } catch(e) {}
 
       const prompt = `Analyse this account and deal situation:
